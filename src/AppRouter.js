6c4fb0395155fb0ae892b2abc39832ab3e1c638b
Ian Kang <seongchank@gmail.com>
2021-12-27 19:59:44 -0800

    Redirect menu page and editor page to login page when user object does not exist

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import BaseLayout from "./Components/BaseLayout/BaseLayout";
-import Home from "./Components/Home/Home";
 import MenuEditorHomeView from "./Components/MenuEditorHomeView/MenuEditorHomeView";
 import MenuHomeView from "./Components/MenuHomeView/MenuHomeView";
 import Login from "./Components/Login/Login";
+import { UserContext } from "./App";
 
 function AppRouter({ authService, menuRepository, imageRepository }) {
+  const { user } = useContext(UserContext);
+
   return (
     <HashRouter>
       <Routes>
         <Route path="/login" element={<Login authService={authService} />} />
-        <Route
-          path="/editor"
-          element={
-            <BaseLayout
-              authService={authService}
-              component={
-                <MenuEditorHomeView
-                  menuRepository={menuRepository}
-                  imageRepository={imageRepository}
+        {user && (
+          <>
+            <Route
+              path="/editor"
+              element={
+                <BaseLayout
+                  authService={authService}
+                  component={
+                    <MenuEditorHomeView
+                      menuRepository={menuRepository}
+                      imageRepository={imageRepository}
+                    />
+                  }
                 />
               }
-            />
-          }
-        ></Route>
-        <Route
-          path="/menu"
-          element={
-            <BaseLayout
-              authService={authService}
-              component={<MenuHomeView menuRepository={menuRepository} />}
-            />
-          }
-        ></Route>
+            ></Route>
+            <Route
+              path="/menu"
+              element={
+                <BaseLayout
+                  authService={authService}
+                  component={<MenuHomeView menuRepository={menuRepository} />}
+                />
+              }
+            ></Route>
+          </>
+        )}
+
         <Route path="*" element={<Login authService={authService} />} />
       </Routes>
     </HashRouter>
