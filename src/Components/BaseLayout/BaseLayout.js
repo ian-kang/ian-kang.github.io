6c0669aee63638a8a61a4cb7ba61fc53e516b58b
Ian Kang <seongchank@gmail.com>
2021-12-27 20:07:02 -0800

    Add Footer

--- src/Components/BaseLayout/BaseLayout.js
+++ src/Components/BaseLayout/BaseLayout.js
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
 function BaseLayout({ authService, component }) {
   return (
@@ -8,6 +9,9 @@ function BaseLayout({ authService, component }) {
         <NavBar authService={authService} />
       </Box>
       <Box>{component}</Box>
+      <Box sx={{ mb: 8 }}>
+        <Footer />
+      </Box>
     </Box>
   );
 }
