f8ceb282d1f64ea23007dd9a4360d0bc8bac5328
Ian Kang <seongchank@gmail.com>
2022-01-12 12:21:30 -0800

    Add redirect from profile page to editor page when the account is first created

--- src/Components/ProfileView/ProfileView.js
+++ src/Components/ProfileView/ProfileView.js
@@ -130,240 +130,242 @@ export default function ProfileView({
     }
   };
   return (
-    <>
-      {customerInfo && (
-        <Grid container justifyContent="center">
-          <Grid item xs={10} sx={{ textAlign: "center" }}>
-            <Typography variant="h5">Profile</Typography>
+    <Grid container justifyContent="center">
+      <Grid item xs={10} sx={{ textAlign: "center" }}>
+        <Typography variant="h5">Profile</Typography>
+      </Grid>
+      {customerInfo ? (
+        <Grid container item spacing={2} sx={{ p: 4, maxWidth: "800px" }}>
+          <Grid item container xs={12} justifyContent="center">
+            <Grid item>
+              <Link to={`/menu/${customerInfo.publicUrl}`} target="_blank">
+                Public Menu URL
+              </Link>
+            </Grid>
           </Grid>
-          <Grid container item spacing={2} sx={{ p: 4, maxWidth: "800px" }}>
-            <Grid item container xs={12} justifyContent="center">
+          <Grid item xs={12}>
+            <Typography>Business Name</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="Business Name"
+              name="businessName"
+              onChange={handleOnChange}
+              defaultValue={customerInfo.name}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>Business Description (Bio)</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="Business Description (Bio)"
+              name="businessDesc"
+              onChange={handleOnChange}
+              defaultValue={customerInfo.desc}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>Location</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="Address Line1"
+              name="addressLine1"
+              onChange={handleOnChange}
+              defaultValue={
+                customerInfo.address && customerInfo.address.addressLine1
+              }
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="Address Line2"
+              name="addressLine2"
+              onChange={handleOnChange}
+              defaultValue={
+                customerInfo.address && customerInfo.address.addressLine2
+              }
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="City"
+              name="city"
+              onChange={handleOnChange}
+              defaultValue={customerInfo.address && customerInfo.address.city}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="State"
+              name="state"
+              onChange={handleOnChange}
+              defaultValue={customerInfo.address && customerInfo.address.state}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              variant="outlined"
+              label="ZIP Code"
+              name="zipCode"
+              onChange={handleOnChange}
+              defaultValue={
+                customerInfo.address && customerInfo.address.zipCode
+              }
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>Phone</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled={loading}
+              fullWidth
+              type="tel"
+              variant="outlined"
+              label="Phone"
+              name="phone"
+              onChange={handleOnChange}
+              defaultValue={customerInfo.phone}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>Logo</Typography>
+          </Grid>
+          {(imageFileOnEdit || customerInfo.logo) && (
+            <Grid
+              container
+              item
+              xs={12}
+              justifyContent="center"
+              alignItems="center"
+            >
               <Grid item>
-                <Link to={`/menu/${customerInfo.publicUrl}`} target="_blank">
-                  Public Menu URL
-                </Link>
-              </Grid>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>Business Name</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="Business Name"
-                name="businessName"
-                onChange={handleOnChange}
-                defaultValue={customerInfo.name}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>Business Description (Bio)</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="Business Description (Bio)"
-                name="businessDesc"
-                onChange={handleOnChange}
-                defaultValue={customerInfo.desc}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>Location</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="Address Line1"
-                name="addressLine1"
-                onChange={handleOnChange}
-                defaultValue={
-                  customerInfo.address && customerInfo.address.addressLine1
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="Address Line2"
-                name="addressLine2"
-                onChange={handleOnChange}
-                defaultValue={
-                  customerInfo.address && customerInfo.address.addressLine2
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="City"
-                name="city"
-                onChange={handleOnChange}
-                defaultValue={customerInfo.address && customerInfo.address.city}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="State"
-                name="state"
-                onChange={handleOnChange}
-                defaultValue={
-                  customerInfo.address && customerInfo.address.state
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                variant="outlined"
-                label="ZIP Code"
-                name="zipCode"
-                onChange={handleOnChange}
-                defaultValue={
-                  customerInfo.address && customerInfo.address.zipCode
-                }
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>Phone</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                disabled={loading}
-                fullWidth
-                type="tel"
-                variant="outlined"
-                label="Phone"
-                name="phone"
-                onChange={handleOnChange}
-                defaultValue={customerInfo.phone}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>Logo</Typography>
-            </Grid>
-            {(imageFileOnEdit || customerInfo.logo) && (
-              <Grid
-                container
-                item
-                xs={12}
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Grid item>
-                  <img
-                    src={imageFileOnEdit || customerInfo.logo}
-                    alt=""
-                    loading="lazy"
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                </Grid>
-              </Grid>
-            )}
-            <Grid item xs={12}>
-              <label htmlFor={`contained-button-file-logo`}>
-                <Input
-                  sx={{ display: "none" }}
-                  type="file"
-                  inputProps={{ accept: "image/*" }}
-                  id={`contained-button-file-logo`}
-                  name="img"
-                  onChange={handleLogoOnChange}
-                  disabled={loading}
+                <img
+                  src={imageFileOnEdit || customerInfo.logo}
+                  alt=""
+                  loading="lazy"
+                  style={{ width: "100%", height: "100%" }}
                 />
-                <Button
-                  variant="contained"
-                  component="span"
-                  startIcon={<Add />}
-                  disabled={loading}
-                >
-                  Upload Logo
-                </Button>
-              </label>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Typography>Business Hours</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              business hours settings
-            </Grid>
-            <Grid item xs={12}>
-              <Typography>Website</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                type="url"
-                variant="outlined"
-                label="Website"
-                name="website"
-                onChange={handleOnChange}
+          )}
+          <Grid item xs={12}>
+            <label htmlFor={`contained-button-file-logo`}>
+              <Input
+                sx={{ display: "none" }}
+                type="file"
+                inputProps={{ accept: "image/*" }}
+                id={`contained-button-file-logo`}
+                name="img"
+                onChange={handleLogoOnChange}
                 disabled={loading}
-                defaultValue={customerInfo.website}
               />
-            </Grid>
-
-            {user && (
-              <>
-                <Grid item xs={12}>
-                  <Typography>Email</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    disabled
-                    fullWidth
-                    variant="outlined"
-                    type="email"
-                    label="Email"
-                    name="email"
-                    defaultValue={user.email}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography>Password</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    disabled
-                    fullWidth
-                    variant="outlined"
-                    label="Password"
-                    type="password"
-                    name="password"
-                  />
-                </Grid>
-              </>
-            )}
-
-            <Grid item xs={12}>
-              <LoadingButton
-                disabled={disabled}
-                loading={loading}
-                fullWidth
+              <Button
                 variant="contained"
-                startIcon={<Save />}
-                onClick={handleSaveButtonOnClick}
+                component="span"
+                startIcon={<Add />}
+                disabled={loading}
               >
-                Save
-              </LoadingButton>
-            </Grid>
+                Upload Logo
+              </Button>
+            </label>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>Business Hours</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            business hours settings
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>Website</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              type="url"
+              variant="outlined"
+              label="Website"
+              name="website"
+              onChange={handleOnChange}
+              disabled={loading}
+              defaultValue={customerInfo.website}
+            />
+          </Grid>
+
+          {user && (
+            <>
+              <Grid item xs={12}>
+                <Typography>Email</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled
+                  fullWidth
+                  variant="outlined"
+                  type="email"
+                  label="Email"
+                  name="email"
+                  defaultValue={user.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography>Password</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  disabled
+                  fullWidth
+                  variant="outlined"
+                  label="Password"
+                  type="password"
+                  name="password"
+                />
+              </Grid>
+            </>
+          )}
+
+          <Grid item xs={12}>
+            <LoadingButton
+              disabled={disabled}
+              loading={loading}
+              fullWidth
+              variant="contained"
+              startIcon={<Save />}
+              onClick={handleSaveButtonOnClick}
+            >
+              Save
+            </LoadingButton>
           </Grid>
         </Grid>
+      ) : (
+        <Grid item sx={{ p: 4 }}>
+          This page will be available after your first menu is created. please
+          follow this link to create your first menu (
+          <Link to="/editor">Menu Editor</Link>)
+        </Grid>
       )}
-    </>
+    </Grid>
   );
 }
